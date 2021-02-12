940f597cc55df57a53eb22d15a5cd7d4b9458e97
Markus <markus.wepper@gmail.com>
2021-02-12 17:11:19 +0100

    update struture

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,30 @@
+/* tslint:disable:no-trailing-whitespace */
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false})nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false})amountInputRef: ElementRef;
+  /* 1. identique choix 2. on met les attribus ou l'objet directement
+   @Output()ingredientAdded = new EventEmitter<{name: string, amount: number}>();*/
+  // 2.
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor() { }
+
+  ngOnInit() { }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    // 1. : const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value,this.amountInputRef.nativeElement.value);
+    // 2. :
+    const newIngredient = new Ingredient(ingName,ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+}
